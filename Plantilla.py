7060da3df27954bd460a24d075a2b8fcddf3df85
carlajl14 <113455597+carlajl14@users.noreply.github.com>
2025-03-11 11:27:22 +0000

    Template

---
+++ Plantilla.py
@@ -0,0 +1,85 @@
+class Estrategia:
+    """
+        Para cargar las distintas estrategias
+    """
+
+    __version__ = 1.0
+
+    def __init__(self) -> None:
+        """
+        Constructor
+
+        Parámetros
+        ..........
+
+        Salida
+        ......
+        return:
+        """
+
+        # Atributos
+
+        # Atributos Privados
+    
+    def __repr__(self) -> str:
+        return self.__class__.__name__ + ".class"
+    
+    def backtest(self):
+        """
+        .....
+
+        Parámetros
+        ..........
+
+        Salida
+        ......
+        return:
+        """
+    
+    # Calcular
+    def calcular(self):
+         """
+        .....
+
+        Parámetros
+        ..........
+
+        Salida
+        ......
+        return:
+        """
+        # Calcular
+
+        return
+    
+    # Optimizar
+    def optimizar(self):
+         """
+        .....
+
+        Parámetros
+        ..........
+
+        Salida
+        ......
+        return:
+        """
+        # Optimizar
+
+        return
+    
+    # Plot
+    def plot(self):
+         """
+        .....
+
+        Parámetros
+        ..........
+
+        Salida
+        ......
+        return:
+        """
+        # Calcular
+
+        return
